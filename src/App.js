aa57bc07ffccd2fa8c7d13ca01dbfc17aab4be5d
debangi <debangiofficial@gmail.com>
2022-05-07 22:35:31 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Chat from './components/Chat';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className='app'>
-      <Header />
-      {/* Header */}
+      <Routes>
+        <Route path='/' element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path='/chat' element={<Chat />} />
+        </Route>
+      </Routes>
       {/* Tinder Cards */}
       {/* Buttons below Tinder cards */}
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PersonIcon from '@mui/icons-material/Person';
 import ForumIcon from '@mui/icons-material/Forum';
 import IconButton from '@mui/material/IconButton';
 
 import './Header.css';
+import { Outlet } from 'react-router-dom';
 
 function Header() {
   return (
-    <div className='header'>
-      <IconButton>
-        <PersonIcon className='header__icon' fontSize='large' />
-      </IconButton>
-      <img
-        className='header__logo'
-        src='https://media.designrush.com/inspiration_images/136074/conversions/_1513706351_928_tinder1_bacbf6370ec8-desktop.jpg'
-        alt='tinder logo'
-      />
-      <IconButton>
-        <ForumIcon className='header__icon' fontSize='large' />
-      </IconButton>
-    </div>
+    <Fragment>
+      <div className='header'>
+        <IconButton>
+          <PersonIcon className='header__icon' fontSize='large' />
+        </IconButton>
+        <img
+          className='header__logo'
+          src='https://media.designrush.com/inspiration_images/136074/conversions/_1513706351_928_tinder1_bacbf6370ec8-desktop.jpg'
+          alt='tinder logo'
+        />
+        <IconButton>
+          <ForumIcon className='header__icon' fontSize='large' />
+        </IconButton>
+      </div>
+      <Outlet />
+    </Fragment>
   );
 }
 
